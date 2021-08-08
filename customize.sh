289a7ee652c1a08b81a51eb070bdfc46d4aa8a5b
awalol <awalol@awaloldeiMac.local>
2021-08-08 14:41:16 +0800

    Initial commit

---
+++ customize.sh
@@ -0,0 +1,6 @@
+# This is customize the module installation process if you need
+print_modname() {
+  ui_print "*******************************"
+  ui_print "        EMUI8 全面屏手势        "
+  ui_print "*******************************"
+}
